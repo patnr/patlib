432c3248d3b1f971c24e36360b6e8602a085c4a1
patricknraanes <patrick.n.raanes@gmail.com>
2020-12-02 23:20:07 +0100

    rm JsonDict, Timer

--- misc.py
+++ misc.py
@@ -86,37 +86,3 @@ try:
         return d
 except ImportError:
     pass
-
-
-import json
-class JsonDict(dict):
-    """Provide json pretty-printing"""
-    def __str__(self): return repr(self)
-    def __repr__(self):
-        s = json.dumps(self, indent=4, sort_keys=False, default=str)
-        crop = lambda t: t[:80] + ("" if len(t)<80 else "...")
-        s = "\n".join([crop(ln) for ln in s.split("\n")])
-        return s
-
-
-import time
-class Timer():
-    """Timer.
-
-    Example::
-
-      with Timer('<description>'):
-        do_stuff()
-    """
-    def __init__(self, name=None):
-        self.name = name
-
-    def __enter__(self):
-        self.tstart = time.time()
-
-    def __exit__(self, type, value, traceback):
-        #pass # Turn off timer messages
-        if self.name:
-            print('[%s]' % self.name, end='')
-        print('Elapsed: %s' % (time.time() - self.tstart))
-
