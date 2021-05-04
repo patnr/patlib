522cbec90340c7aa694dccad1edd76419e0dd579
patricknraanes <patrick.n.raanes@gmail.com>
2021-05-04 11:17:45 +0200

    Enable py37 again

--- patlib/__init__.py
+++ patlib/__init__.py
@@ -8,9 +8,10 @@ It may be loaded with wildcard, eg:
 - from patlib.math import * # won't execute the other submodules
 """
 
-from importlib.metadata import version
-
 try:
-    __version__ = version(__name__)
-except:
-    pass
+    import importlib.metadata as importlib_metadata
+except ModuleNotFoundError:
+    import importlib_metadata  # type: ignore
+
+# https://github.com/python-poetry/poetry/pull/2366#issuecomment-652418094
+__version__ = importlib_metadata.version(__name__)
